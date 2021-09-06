f8c2cb1fddab56141bb665b040cc09832792938c
Haru02w <joaovicmillane@gmail.com>
2021-09-05 22:56:53 -0300

    account.php arrumado

--- account.php
+++ account.php
@@ -59,7 +59,7 @@
                     $rand = rand(0, 99999);
                     $encname = $date . $rand;
                     $filename = md5($encname) . '.' . $fileext;
-                    $filepath = '/allimages/' . $filename;
+                    $filepath = 'allimages/' . $filename;
                     
                     try {
                         if(!move_uploaded_file($temp, $filepath)) {
