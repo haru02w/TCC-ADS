c16c40889af173577ed32c4ac2362ba95321b7e2
Sabinis3 <joaovitorrsabino@gmail.com>
2021-09-19 20:47:00 -0300

    Correção do erro de repetição.

--- developmentservices.php
+++ developmentservices.php
@@ -99,12 +99,6 @@ mysqli_close($conn);
                                             <?php } ?>
                                         </footer>
                                     </div>
-                                    <footer class="card-footer">
-                                        <a href="/details/<?php echo $rowser['ID_SERVICE'];?>" class="card-footer-item">Ver detalhes</a>
-                                        <?php if($type == "CUSTOMER") { ?>
-                                            <a href="/updateservice/<?php echo $rowser['ID_SERVICE'];?>/" class="card-footer-item">Editar serviço</a>
-                                        <?php } ?>
-                                    </footer>
                                 </div>
                             <?php } ?>
                         </div>
@@ -124,6 +118,9 @@ mysqli_close($conn);
             methods: {
                 onClickBurger() {
                     this.isActiveBurger = !this.isActiveBurger
+                },
+                onClickLogout() {
+                    window.location.replace("/logout/")
                 },
             }
         })
